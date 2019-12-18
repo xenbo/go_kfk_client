f8d6f2dd48599e186f34cca14280e0c8117cc2b8
dongbo <dongbo@localhost.localdomain>
2019-12-18 10:59:49 +0800

    add mod

---
+++ example/db_example/test_db.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	rdkfk "github.com/xenbo/go_kfk_client/rdkfk"
+	"strconv"
+)
+
+func main() {
+
+	rdkfk.Cgo_init()
+
+	db := rdkfk.Cgo_createdb("testdb", 10000)
+
+	var key string = "key_"
+	var val string = "val_asdfasdfdsafasdfasdfasdfasdfasikodkfjklashdfjkasdhfk" +
+		"jashdfjhasdjkfhajksdhfjkasdhfjkasdhfjkdashjfhasdjkfhasjkdfjaksd" +
+		"hfjkasdhfjkasdhfjkasdfjkhasdjkasdfhjkasdhfjkasd"
+
+	for i := 0; i < 1000000; i++ {
+		var val1 string = val + strconv.Itoa(i)
+		var key1 string = key + strconv.Itoa(i)
+		rdkfk.Cgo_setkey(db, key1, val1)
+	}
+
+	for i := 0; i < 1000000; i++ {
+		var key string = "key_"
+		var key1 string = key + strconv.Itoa(i)
+
+		v := rdkfk.Cgo_getkey(db, key1)
+		fmt.Println(v)
+	}
+}
---
+++ example/go.mod
@@ -0,0 +1,3 @@
+module github.com/xenbo/go_kfk_client/rdkfk
+
+go 1.13
